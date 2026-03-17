76eb1623424831a772745da7e97ea9ea531203e7
Mohamed Emara <memaraaa123@gmail.com>
2026-03-17 04:59:17 +0200

    done

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
         babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
